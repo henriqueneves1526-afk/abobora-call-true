84e802a27432c7a5fb52f955c0cb486243986fc4
henriqueneves1526-afk <henriqueneves1526@gmail.com>
2025-12-13 21:50:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -83,5 +83,6 @@ function matchUsers() {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
-  console.log(`🔗 Teste: http://localhost:${PORT}/test`);
+  console.log(`🔗 Teste: https://abobora-call-true.onrender.comt`);
+
 });
